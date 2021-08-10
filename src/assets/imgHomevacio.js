420bcb6b0bcfee6896b6d263f4d293013dbd3219
JaiderCarvajal <jaider.carvajal@gmail.com>
2021-08-10 18:43:08 -0500

    Complemento código

---
+++ src/assets/imgHomevacio.js
@@ -0,0 +1,14 @@
+import html5vacio from './img/html5vacio.png';
+import css3vacio from './img/css3vacio.png';
+import jsvacio from './img/jsvacio.png';
+import figmavacio from './img/figmavacio.png';
+import uxvacio from './img/uxvacio.png';
+
+
+export default {
+    "img1": html5vacio, 
+    "img2": css3vacio,
+    "img3": jsvacio,
+    "img4": figmavacio,
+    "img5": uxvacio
+}
